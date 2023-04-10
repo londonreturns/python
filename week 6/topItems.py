ab7448d2b5969120dac9863f727dd1f3aa4b0c62
londonreturns <subedioscar.47@gmail.com>
2023-04-10 14:45:44 +0545

    top three updated

--- week 6/topItems.py
+++ week 6/topItems.py
@@ -1,16 +1,14 @@
 # top items
 
+def topThreeItems(maxItemsDic):
+    maxItemsDic = (sorted(maxItemsDic.items(), key=lambda item: item[1]))
+    print("Top three items are", maxItemsDic[2:5])
+
+
 priceOfItem = {'item1': 45.50,
                'item2': 35,
                'item3': 41.30,
                'item4': 55,
                'item5': 24}
 
-maxPrice = 0
-maxItem = ""
-for item, prices in priceOfItem.items():
-    if maxPrice < prices:
-        maxPrice = prices
-        maxItem = item
-
-print("Highest price item is", maxItem, maxPrice)
+topThreeItems(priceOfItem)
